af6c884206de9b3acfe891ec0fb4ff226504025c
Hfragoso <heberfm@hotmail.com>
2017-09-20 22:25:42 -0400

    comit with DaggerNetComponent not working

---
+++ app/src/main/java/com/example/heber/abercrombiefitch/mainscreen/App.java
@@ -0,0 +1,24 @@
+package com.example.heber.abercrombiefitch.mainscreen;
+
+import android.app.Application;
+
+import com.example.heber.abercrombiefitch.data.component.NetComponent;
+import com.example.heber.abercrombiefitch.data.module.AppModule;
+import com.example.heber.abercrombiefitch.data.module.NetModule;
+
+public class App extends Application {
+    private NetComponent mNetComponent;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mNetComponent = DaggerNetComponent.builder()
+                .appModule(new AppModule(this))
+                .netModule(new NetModule("http://jsonplaceholder.typicode.com/"))
+                .build();
+    }
+
+    public NetComponent getNetComponent() {
+        return mNetComponent;
+    }
+}
